8d81709c694fb4f5f9d7f56328288c00be145665
Axel Straube & Cory Spitzer <coryspitzer@gmail.com>
2015-04-30 16:26:30 -0700

    Go to Address Book after submitting information on index form

--- app.rb
+++ app.rb
@@ -2,7 +2,22 @@ require('./lib/contact')
 require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/*.rb')
+# require('pry')
 
 get('/') do
   erb(:index)
+end
+
+post('/address_book') do
+  first_name = params.fetch('first_name')
+  last_name = params.fetch('last_name')
+  birthday = params.fetch('birthday')
+  phone = params.fetch('phone')
+  email = params.fetch('email')
+  @person = Person.new({:first_name => first_name, :last_name => last_name})
+  erb(:address_book)
+end
+
+get('/address_book') do
+  erb(:address_book)
 end
--- spec/address_book_integration_spec.rb
+++ spec/address_book_integration_spec.rb
@@ -2,9 +2,19 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 
-describe('the root path', {:type => :feature}) do
-  it('visits index') do
-    visit('/')
-    expect(page).to(have_content("Address Book"))
+describe('the app paths', {:type => :feature}) do
+  describe('the root path') do
+    it('visits index') do
+      visit('/')
+      expect(page).to(have_content("Address Book"))
+    end
   end
+
+  describe('the address book path') do
+    it('clicks on address book button and visits address book') do
+      visit('/address_book')
+      expect(page).to(have_content('This is the address book!'))
+    end
+  end
+
 end
