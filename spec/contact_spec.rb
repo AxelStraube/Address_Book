3bb3436259a9b6431679199abc9c932c23220c20
Axel Straube & Cory Spitzer <coryspitzer@gmail.com>
2015-04-30 09:36:53 -0700

    Fix initialize test

--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -4,7 +4,7 @@ require('contact')
 describe(Contact) do
     describe('#initialize') do
       it('Initialize a new Contact') do
-        expect(Contact.new('Axel')).to(eq(not(nil)))
+        expect(Contact.new('Axel').nil?).to(eq(false))
       end
     end
 end
