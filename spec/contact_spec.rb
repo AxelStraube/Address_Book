37c1dc8cde277cd62fa5c173278d57fbec9807c0
Axel Straube & Cory Spitzer <coryspitzer@gmail.com>
2015-04-30 09:43:20 -0700

    Test name method

--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -3,8 +3,16 @@ require('contact')
 
 describe(Contact) do
     describe('#initialize') do
-      it('Initialize a new Contact') do
+      it('initializes a new Contact') do
         expect(Contact.new('Axel').nil?).to(eq(false))
+      end
+    end
+
+    describe('#name') do
+      it('sets and gets name') do
+        person = Contact.new('Cory')
+        person.name = "John"
+        expect(person.name).to(eq('John'))
       end
     end
 end
