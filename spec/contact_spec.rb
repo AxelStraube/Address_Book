0c12d83452da542dd3968b4d98442910b28038b0
Axel Straube & Cory Spitzer <coryspitzer@gmail.com>
2015-04-30 10:27:00 -0700

    Test for a hash argument

--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -4,13 +4,13 @@ require('contact')
 describe(Contact) do
     describe('#initialize') do
       it('initializes a new Contact') do
-        expect(Contact.new('Axel', 'Straube').nil?).to(eq(false))
+        expect(Contact.new({:first_name => 'Axel', :last_name => 'Straube'}).nil?).to(eq(false))
       end
     end
 
     describe('#first_name') do
       it('sets and gets first name') do
-        person = Contact.new('Cory', 'Spitzer')
+        person = Contact.new({:first_name => 'Cory', :last_name =>'Spitzer'})
         person.first_name = "John"
         expect(person.first_name).to(eq('John'))
       end
@@ -18,7 +18,7 @@ describe(Contact) do
 
     describe('#last_name') do
       it('sets and gets last name') do
-        person = Contact.new('John', 'Smith')
+        person = Contact.new({:first_name => 'John', :last_name => 'Smith'})
         person.last_name = 'Johnson'
         expect(person.last_name).to(eq('Johnson'))
       end
@@ -26,7 +26,7 @@ describe(Contact) do
 
     describe('#birthday') do
       it ('sets and gets a birthday') do
-        person = Contact.new('Alfred', 'Johnson')
+        person = Contact.new({:first_name => 'Alfred', :last_name => 'Johnson'})
         person.birthday = Time.new(1955, 04, 30)
         expect(person.birthday.to_s).to(eq("1955-04-30 00:00:00 -0700"))
       end
