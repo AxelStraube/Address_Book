555d9b2f453430c5e09d8f2aec2e310a3d58b330
Axel Straube & Cory Spitzer <coryspitzer@gmail.com>
2015-04-30 10:27:59 -0700

    Add birthday method and switch initialize parameter to a hash

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,8 +1,13 @@
 class Contact
-  attr_accessor :first_name, :last_name
+  attr_accessor :first_name, :last_name, :birthday
 
-  define_method(:initialize) do |first_name, last_name|
-    @first_name = first_name
-    @last_name = last_name
+  define_method(:initialize) do |attributes|
+    @first_name = attributes.fetch(:first_name)
+    @last_name = attributes.fetch(:last_name)
+    if attributes.has_key?(:birthday)
+      @birthday = attributes.fetch(:birthday)
+    else
+      @birthday = nil
+    end
   end
 end
